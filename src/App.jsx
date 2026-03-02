8b4427be9e078708d6cfad787481ad8ac28f8d01
Your Name <pranavmohanindeevaram@gmail.com>
2026-03-02 23:01:00 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
 
       {!showWelcome && (
         <>
-          <Navbar />
+          
           <AnimatedBackground />
           <Home />
           <About />
